d7a11b14cf5eb9c7f1ff08e9973e6e98b46c3123
ors-reyes <ors2323@gmail.com>
2025-01-11 05:00:41 -0300

    nav

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './app.css'
 import { useRef, useEffect, useState } from 'react'
 import useIntersetion from './components/useIntersetion'
+import Menu from './components/navbar/menu'
 
 const App = () => {
   const [elementoRef, isIntersecting] = useIntersetion({
@@ -11,14 +12,15 @@ const App = () => {
   })
   return (
     <>
+      <Menu/>
       <section><h1 className='a'>App a-01</h1></section>
       <section><h1 className='b'>App b-02</h1></section>
-      <section ref={elementoRef2}><h1 className='c'>{isIntersecting2 ? "dentro" : "fuera"}</h1></section>
+      <section ref={elementoRef2}><h1 className='c'>{isIntersecting2 ? "dentro -1" : "fuera -1"}</h1></section>
       <section><h1 className='d'>App d-04</h1></section>
       <section><h1 className='e'>App e-05</h1></section>
       <section><h1 className='f'>App f-06</h1></section>
       <section><h1 className='g'>App g-07</h1></section>
-      <section ref={elementoRef}><h1 className='h'>{isIntersecting ? "dentro" : "fuera"}</h1></section>
+      <section ref={elementoRef}><h1 className='h'>{isIntersecting ? "dentro -2" : "fuera -2"}</h1></section>
       <section><h1 className='i'>App i-09</h1></section>
       <section><h1 className='j'>App j-10</h1></section>
     </>
---
+++ src/components/navbar/Menu.jsx
@@ -0,0 +1,22 @@
+import './menu.css'
+
+const Menu = () => {
+  return (
+    <>
+        <div className='navbar'>
+            <div className='logo'></div>
+            <nav className='nav'>
+                <ul>
+                    <li>APLICACIÓN</li>
+                    <li>PREPARACIÓN</li>
+                    <li>RENDIMIENTO</li>
+                    <li>VARIEDAD</li>
+                    <li>FAQ</li>
+                </ul>
+            </nav>
+        </div>      
+    </>
+  )
+}
+
+export default Menu
